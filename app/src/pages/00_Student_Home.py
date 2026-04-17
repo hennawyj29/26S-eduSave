9f03c5c88c685c6d3f7b22a7b502fbbb9baa8bf8
Julianne Conlee <julianneconlee@Juliannes-MacBook-Air.local>
2026-04-17 13:18:18 -0400

    updates to 00_Student_Home.py

---
+++ app/src/pages/00_Student_Home.py
@@ -0,0 +1,28 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout='wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome International College Student, {st.session_state['first_name']}.")
+st.write('### What would you like to do today?')
+
+if st.button('Grocery Discount Search',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/01_World_Bank_Viz.py')
+
+if st.button('Clothing Discount Search ',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/01_World_Bank_Viz.py')
+
+if st.button('My Deals',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/02_Map_Demo.py')
