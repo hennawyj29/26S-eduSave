801aaafd3ee4d2aacdf7013a6a40cbe033b9f44e
hennawyj29 <hennawy.j@northeastern.edu>
2026-04-18 02:17:03 -0400

    added Sofia home page

---
+++ app/src/pages/20_Sophia_Home.py
@@ -0,0 +1,33 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout='wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome, {st.session_state['first_name']}!")
+st.write('### What would you like to do today?')
+
+if st.button('View Listing Analytics',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/21_Listing_Analytics.py')
+
+if st.button('View Traffic Snapshots',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/22_Traffic_Snapshots.py')
+
+if st.button('View Competitor Listings',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/23_Competitor_Listings.py')
+
+if st.button('Manage My Discounts',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/24_Manage_Discounts.py')
